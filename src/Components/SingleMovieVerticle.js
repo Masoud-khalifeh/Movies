9cc5a708f13f0a8ac8e70435883ac30e20df583b
Masoud Khalifeh <masoud.khalifeh28@gmail.com>
2023-08-09 01:49:25 +0200

    add TopRated and SingleMovie components

---
+++ src/Components/SingleMovieVerticle.js
@@ -0,0 +1,29 @@
+import React from "react";
+import '../Styles/SingleMovieVerticle.css';
+import { FaStar } from "react-icons/fa";
+import { BiLinkExternal } from "react-icons/bi";
+import { BsFillPlayFill, BsFillBookmarkFill, BsPlus } from "react-icons/bs";
+
+
+export default function SingleMovieVerticle(propos) {
+
+    return (
+        <div className="singleMovieContainer">
+            <div className="top">
+                <div className="bookMark"><BsFillBookmarkFill className="icon"/><BsPlus className="iconPlus"/></div>
+                <img src={propos.img} className="image" />
+            </div>
+            <div className="bottom">
+                <div className="text">
+                    <p className="rate"><FaStar className="icon"/> {propos.rate}</p>
+                    <p className="title">{propos.title}</p>
+                </div>
+                <div className="button">
+                    <button className="watchNow">Watch Now <BiLinkExternal className="icon"/></button>
+                    <button className="trailor"> <BsFillPlayFill/> Trailor</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
