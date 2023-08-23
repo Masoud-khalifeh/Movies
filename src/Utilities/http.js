945b614c7bf5d930e12129792d3791b322e1cd2d
Masoud Khalifeh <masoud.khalifeh28@gmail.com>
2023-08-24 01:26:57 +0200

    add Password hashing in addUser

--- src/Utilities/http.js
+++ src/Utilities/http.js
@@ -1,19 +1,22 @@
 import axios from "axios";
+import bcrypt from "bcryptjs";
 
-const url="https://uiux-expert.de/movies/php/"
+const url = "https://uiux-expert.de/movies/php/";
 
 
 export async function addUser(name, email, password) {
+    const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync());
+    console.log(hashedPassword);
     try {
         let fData = new FormData();
         fData.append('name', name);
         fData.append('email', email);
-        fData.append('password', password);
+        fData.append('password', hashedPassword);
 
         const result = await axios.post(`${url}add_user.php`, fData)
-        .then(response => JSON.parse(response.data));
-        
-        console.log("res",result)
+            .then(response => JSON.parse(response.data));
+
+        console.log("res", result)
         return result;
     } catch (error) {
         return error.toString();
